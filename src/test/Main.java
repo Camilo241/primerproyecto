55f21be85891cae495ec3b8646857b1a6b8da2f1
Kmilo160702 <camilo160702@hotmail.com>
2023-09-23 21:57:53 -0500

    Fix bug pomodoro value :bug:

--- src/test/Main.java
+++ src/test/Main.java
@@ -4,6 +4,6 @@ import src.main.java.com.ListaTareas.control.Pomodoro;
 
 public class Main {
     public static void main(String[] args) {
-        Pomodoro pomodoro = new Pomodoro();
+        new Pomodoro();
     }
 }
