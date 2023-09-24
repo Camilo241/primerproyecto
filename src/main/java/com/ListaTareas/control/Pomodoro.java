dd31c8556e00bd59a81055276f74888b1fa543ad
Camilo241 <cristiancamilofandinomendieta@gmail.com>
2023-09-23 21:22:40 -0500

    Pomodoro temporal :bowtie:

--- src/main/java/com/ListaTareas/control/Pomodoro.java
+++ src/main/java/com/ListaTareas/control/Pomodoro.java
@@ -3,19 +3,50 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Pomodoro {
-        private Timer timer;
-        private int minutos;
-        private boolean comienza;
+    private static final int WORK_DURATION = 25 * 60;  // Duración del trabajo en segundos
+    private static final int BREAK_DURATION = 5 * 60;  // Duración del descanso en segundos
+    private Timer timer;
+    private int remainingTime;
+    private boolean isWorking;
 
-
-    public Pomodoro(int minutes){
-        this.minutos = minutos;
-        this.timer = new Timer();
+    public Pomodoro() {
+        remainingTime = WORK_DURATION;
+        isWorking = true;
+        createTimer();
     }
 
+    private void createTimer() {
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+                if (remainingTime > 0) {
+                    remainingTime--;
+                    displayTime();
+                } else {
+                    toggleTimerMode();
+                }
+            }
+        }, 0, 1000); // Actualiza cada 1 segundo
+    }
 
-    public void detener() {
-        timer.cancel();
-        comienza = false;
+    private void toggleTimerMode() {
+        isWorking = !isWorking;
+        if (isWorking) {
+            remainingTime = WORK_DURATION;
+        } else {
+            remainingTime = BREAK_DURATION;
+        }
+        displayTime();
+    }
+
+    private void displayTime() {
+        int minutes = remainingTime / 60;
+        int seconds = remainingTime % 60;
+        System.out.printf("%02d:%02d - %s%n", minutes, seconds, isWorking ? "Trabajo" : "Descanso");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Comenzando el temporizador Pomodoro");
+        Pomodoro pomodoro = new Pomodoro();
     }
 }
