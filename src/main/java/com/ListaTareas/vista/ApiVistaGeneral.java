9bc1d3b160b4f1d466d44d160d59d625f8d22f29
Camilo241 <cristiancamilofandinomendieta@gmail.com>
2023-10-10 19:49:39 -0500

    Cambios en la Vista :construction:

--- src/main/java/com/ListaTareas/vista/ApiVistaGeneral.java
+++ src/main/java/com/ListaTareas/vista/ApiVistaGeneral.java
@@ -10,39 +10,37 @@ public class ApiVistaGeneral extends JFrame {// inheriting JFrame
     JFrame principal;
 
     ApiVistaGeneral() {
-        JLabel titulo,titulo2,ntarea,pomodoro;
+        JLabel titulo, titulo2, ntarea, pomodoro;
         titulo = new JLabel("Lista de Tareas");
-        titulo.setBounds(25,50,300,20);
-        titulo2 = new JLabel("Bienvenido "+ getName());
-        titulo2.setBounds(25,75,300,20);
+        titulo.setBounds(25, 50, 300, 20);
+        titulo2 = new JLabel("Bienvenido " + getName());
+        titulo2.setBounds(25, 75, 300, 20);
         pomodoro = new JLabel("Pomodoro");
-        pomodoro.setBounds(25,100,300,150);
-
+        pomodoro.setBounds(25, 100, 300, 150);
 
         JPanel panel;
 
-        panel=new JPanel();  
-        panel.setBounds(40,120,300,200);    
+        panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setBounds(40, 120, 300, 200);
         panel.setBackground(Color.GRAY);
 
         JTextField t1, t2;
         ntarea = new JLabel("Numero de la tarea");
-        ntarea.setBounds(40,100,300,20);
+        ntarea.setBounds(40, 100, 300, 20);
 
-        t1 = new JTextField("Nombre de La tarea");
-        t1.setBounds(50, 120, 200, 40);
-
+        t1 = new JTextField("Nombre de la tarea");
         t2 = new JTextField("Descripcion");
-        t2.setBounds(50, 150, 200, 30);
 
-        JButton añadirTarea,eliminar,editar;
-        añadirTarea = new JButton("Añadir Tarea");// create button
+
+        JButton añadirTarea, eliminar, editar;
+        añadirTarea = new JButton("Añadir Tarea");
         añadirTarea.setBounds(230, 75, 120, 20);
 
-        eliminar = new JButton("X");// create button
+        eliminar = new JButton("X");
         eliminar.setBounds(230, 150, 120, 20);
 
-        editar = new JButton("Editar tarea");// create button
+        editar = new JButton("Editar tarea");
         editar.setBounds(230, 170, 120, 20);
 
         add(panel);
@@ -50,7 +48,7 @@ public class ApiVistaGeneral extends JFrame {// inheriting JFrame
         panel.add(t2);
         panel.add(pomodoro);
         panel.add(eliminar);
-        panel.add(editar);  
+        panel.add(editar);
         add(titulo);
         add(titulo2);
         add(ntarea);
