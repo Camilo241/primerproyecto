923af3023cc1a4f027b6815a0aaed11155cf47cd
Camilo241 <cristiancamilofandinomendieta@gmail.com>
2023-11-24 13:04:28 -0500

    Eliminacion de Archivos  :fire:

--- src/main/java/com/ListaTareas/control/tarea.java
+++ src/main/java/com/ListaTareas/control/tarea.java
@@ -1,32 +1,18 @@
 package src.main.java.com.ListaTareas.control;
 
-import java.sql.Date;
 
-public class Tarea {
-
-    private int id;
+public class Tarea extends ListaTareas{
     private String nombre_tarea;
     private String descripcion;
     private boolean completada;
-    private String categoria;
-    private int pomodoro;
 
-    public Tarea(int id, String nombre_tarea, String descripcion, boolean completada, String categoria, int pomodoro) {
-        this.id = id;
+
+    public Tarea( String nombre_tarea, String descripcion, boolean completada) {
         this.nombre_tarea = nombre_tarea;
         this.descripcion = descripcion;
         this.completada = completada;
-        this.categoria = categoria;
-        this.pomodoro = pomodoro;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public String getNombre_tarea() {
         return nombre_tarea;
@@ -50,22 +36,6 @@ public class Tarea {
 
     public void setCompletada(boolean completada) {
         this.completada = completada;
-    }
-
-    public String getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
-
-    public int getPomodoro() {
-        return pomodoro;
-    }
-
-    public void setPomodoro(int pomodoro) {
-        this.pomodoro = pomodoro;
     }
 
 }
